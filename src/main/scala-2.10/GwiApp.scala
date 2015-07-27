ca9af5f73351687f422d58f6a6bb4c6098af68e3
Petr Janda <petrjandacz@gmail.com>
2015-07-27 09:51:10 +0100

    cleanup

--- src/main/scala-2.10/GwiApp.scala
+++ src/main/scala-2.10/GwiApp.scala
@@ -2,22 +2,18 @@ import jobs.{EvaluateModel, HeadlessCsvToLabeledPoints, LogisticRegression}
 import ngn.spark._
 
 object GwiApp extends SparkJobApp {
-  args.toList.head match {
-    case "train" => run(for {
-      df <- new HeadlessCsvToLabeledPoints(
-        path = "data/gwi.txt",
-        labelColumn = "q20_1_5"
-      )
+  run(for {
+    df <- new HeadlessCsvToLabeledPoints(
+      path = "data/gwi.txt",
+      labelColumn = "q20_1_5"
+    )
 
-      model <- new LogisticRegression(
-        df = df,
-        featuresColumn = "features",
-        labelColumn = "label"
-      )
+    model <- new LogisticRegression(
+      df = df,
+      featuresColumn = "features",
+      labelColumn = "label"
+    )
 
-      _  <- new EvaluateModel(model)
-    } yield ())
-  }
+    _  <- new EvaluateModel(model)
+  } yield ())
 }
-
-
